d541a6485460bcac5980c8ff6c017043caa19e3b
Ramazanov Vitalii <zapupenec@gmail.com>
2023-02-28 21:18:24 +0300

    add compare flat json files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0')
+  .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
-
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,64 @@
+import fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+const parse = (filepath) => {
+  const absFilepath = path.resolve(filepath);
+  const result = fs.readFileSync(absFilepath, 'utf8');
+  return JSON.parse(result);
+};
+
+const compare = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.sortBy(_.union(keys1, keys2));
+
+  const result = keys.reduce((acc, key) => {
+    if (!Object.hasOwn(data1, key)) {
+      acc[key] = 'added';
+    } else if (!Object.hasOwn(data2, key)) {
+      acc[key] = 'deleted';
+    } else if (data1[key] !== data2[key]) {
+      acc[key] = 'changed';
+    } else {
+      acc[key] = 'unchanged';
+    }
+    return acc;
+  }, {});
+
+  return result;
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = parse(filepath1);
+  const data2 = parse(filepath2);
+
+  const diff = compare(data1, data2);
+  const keys = Object.keys(diff);
+
+  const lines = keys.reduce((acc, key) => {
+    switch (diff[key]) {
+      case 'added':
+        acc.push(`  + ${key}: ${data2[key]}`);
+        break;
+      case 'deleted':
+        acc.push(`  - ${key}: ${data1[key]}`);
+        break;
+      case 'changed':
+        acc.push(`  - ${key}: ${data1[key]}`);
+        acc.push(`  + ${key}: ${data2[key]}`);
+        break;
+      case 'unchanged':
+        acc.push(`    ${key}: ${data1[key]}`);
+        break;
+      default:
+    }
+    return acc;
+  }, []).join('\n');
+
+  return `{\n${lines}\n}`;
+};
+
+export default genDiff;
+
+// const dataFormat = path.extname(filepath1);
