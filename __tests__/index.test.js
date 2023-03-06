c7ad496a021b0282e720f990dd37b6d4208db19d
Ramazanov Vitalii <zapupenec@gmail.com>
2023-03-06 20:05:14 +0300

    add test output plain

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -17,18 +17,31 @@ const examFile = 'examFile.txt';
 const examFilepath = getFixturePath(examFile);
 const examData = readFile(examFile);
 
+const examFilePlain = 'examFilePlain.txt';
+const examFilePlainPath = getFixturePath(examFilePlain);
+const examDataPlain = readFile(examFilePlain);
+
 const pairsForCompare = [
   [jsonFile1, jsonFile2],
   [ymlFile1, yamlFile2],
   [ymlFile1, jsonFile2],
 ];
 
-test('compare', () => {
+test('output default(stylish)', () => {
   pairsForCompare.forEach(([file1, file2]) => {
     const filepath1 = getFixturePath(file1);
     const filepath2 = getFixturePath(file2);
-    const compare = genDiff(filepath1, filepath2);
+    const compare = genDiff(filepath1, filepath2, 'stylish');
     expect(compare).toEqual(examData);
+  });
+});
+
+test('output plain', () => {
+  pairsForCompare.forEach(([file1, file2]) => {
+    const filepath1 = getFixturePath(file1);
+    const filepath2 = getFixturePath(file2);
+    const compare = genDiff(filepath1, filepath2, 'plain');
+    expect(compare).toEqual(examDataPlain);
   });
 });
 
@@ -37,6 +50,6 @@ test('File with incorrect extension', () => {
   let message = `The file '${jsonFile1}' or '${examFile}' with incorrect extension. Use .json or .yml(.yaml) file.`;
   expect(genDiff(filepath1, examFilepath)).toBe(message);
 
-  message = `The file '${examFile}' and '${examFile}' with incorrect extension. Use .json or .yml(.yaml) file.`;
-  expect(genDiff(examFilepath, examFilepath)).toBe(message);
+  message = `The file '${examFilePlain}' and '${examFile}' with incorrect extension. Use .json or .yml(.yaml) file.`;
+  expect(genDiff(examFilePlainPath, examFilepath)).toBe(message);
 });
