3d00eba63bfe62d13f752d5acc22c02a4045b0c5
Ramazanov Vitalii <zapupenec@gmail.com>
2023-03-08 13:38:38 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,28 +6,22 @@ import genDiff from '../src/index.js';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename).trimEnd();
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trimEnd();
 
-const jsonFile1 = 'file1.json';
-const jsonFile2 = 'file2.json';
-const ymlFile1 = 'file1.yml';
-const yamlFile2 = 'file2.yaml';
-
-const examFile = 'examFile.txt';
-const examFilepath = getFixturePath(examFile);
-const examData = readFile(examFile);
-
-const examFilePlain = 'examFilePlain.txt';
-const examFilePlainPath = getFixturePath(examFilePlain);
-const examDataPlain = readFile(examFilePlain);
+const result = {
+  stylish: 'result_stylish.txt',
+  plain: 'result_plain.txt',
+  json: 'result_json.txt',
+};
 
 const pairsForCompare = [
-  [jsonFile1, jsonFile2],
-  [ymlFile1, yamlFile2],
-  [ymlFile1, jsonFile2],
+  ['file1.json', 'file2.json'],
+  ['file1.yml', 'file2.yaml'],
+  ['file1.yml', 'file2.json'],
 ];
 
 test('output default(stylish)', () => {
+  const examData = readFile(result.stylish);
   pairsForCompare.forEach(([file1, file2]) => {
     const filepath1 = getFixturePath(file1);
     const filepath2 = getFixturePath(file2);
@@ -37,19 +31,34 @@ test('output default(stylish)', () => {
 });
 
 test('output plain', () => {
+  const examData = readFile(result.plain);
   pairsForCompare.forEach(([file1, file2]) => {
     const filepath1 = getFixturePath(file1);
     const filepath2 = getFixturePath(file2);
     const compare = genDiff(filepath1, filepath2, 'plain');
-    expect(compare).toEqual(examDataPlain);
+    expect(compare).toEqual(examData);
+  });
+});
+
+test('output json', () => {
+  const examData = readFile(result.json);
+  pairsForCompare.forEach(([file1, file2]) => {
+    const filepath1 = getFixturePath(file1);
+    const filepath2 = getFixturePath(file2);
+    const compare = genDiff(filepath1, filepath2, 'json');
+    expect(compare).toEqual(examData);
   });
 });
 
 test('File with incorrect extension', () => {
-  const filepath1 = getFixturePath(jsonFile1);
-  let message = `The file '${jsonFile1}' or '${examFile}' with incorrect extension. Use .json or .yml(.yaml) file.`;
-  expect(genDiff(filepath1, examFilepath)).toBe(message);
-
-  message = `The file '${examFilePlain}' and '${examFile}' with incorrect extension. Use .json or .yml(.yaml) file.`;
-  expect(genDiff(examFilePlainPath, examFilepath)).toBe(message);
+  const inputsFiles = [
+    ['file1.json', 'result_plain.txt', 'or'],
+    ['result_plain.txt', 'result_json.txt', 'and'],
+  ];
+  inputsFiles.forEach(([file1, file2, preposition]) => {
+    const filepath1 = getFixturePath(file1);
+    const filepath2 = getFixturePath(file2);
+    const message = `The file '${file1}' ${preposition} '${file2}' with incorrect extension. Use .json or .yml(.yaml) file.`;
+    expect(genDiff(filepath1, filepath2)).toBe(message);
+  });
 });
