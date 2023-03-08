41d0ecfe5074175b218d6e948e4b88069e5bf673
Ramazanov Vitalii <zapupenec@gmail.com>
2023-03-08 22:52:46 +0300

    remove mutation in src/compare.js

--- src/compare.js
+++ src/compare.js
@@ -6,26 +6,43 @@ const compare = (data1, data2) => {
   const keys = _.sortBy(_.union(keys1, keys2));
 
   const result = keys.map((key) => {
-    const node = { name: key };
     if (!Object.hasOwn(data1, key)) {
-      node.status = 'added';
-      node.value = data2[key];
-    } else if (!Object.hasOwn(data2, key)) {
-      node.status = 'removed';
-      node.value = data1[key];
-    } else if (data1[key] !== data2[key]) {
-      node.status = 'updated';
-      if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-        node.children = compare(data1[key], data2[key]);
-      } else {
-        node.value1 = data1[key];
-        node.value2 = data2[key];
-      }
-    } else {
-      node.status = 'unchanged';
-      node.value = data1[key];
+      return {
+        name: key,
+        status: 'added',
+        value: data2[key],
+      };
     }
-    return node;
+
+    if (!Object.hasOwn(data2, key)) {
+      return {
+        name: key,
+        status: 'removed',
+        value: data1[key],
+      };
+    }
+
+    if (data1[key] !== data2[key]) {
+      if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+        return {
+          name: key,
+          status: 'updated',
+          children: compare(data1[key], data2[key]),
+        };
+      }
+      return {
+        name: key,
+        status: 'updated',
+        value1: data1[key],
+        value2: data2[key],
+      };
+    }
+
+    return {
+      name: key,
+      status: 'unchanged',
+      value: data1[key],
+    };
   });
 
   return result;
