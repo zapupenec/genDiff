7760d126a759cb126a3b1a027f98b6fa228c6639
Ramazanov Vitalii <zapupenec@gmail.com>
2023-03-13 14:27:18 +0300

    refactor src/formatters/stylysh.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,22 @@
 import _ from 'lodash';
 
+const stringify = (startDepth, value, replacer = ' ', spacesCount = 1) => {
+  const iter = (currentValue, depth) => {
+    if (!_.isObject(currentValue)) {
+      return `${currentValue}`;
+    }
+    const space = replacer.repeat(spacesCount * depth);
+    const bracketSpace = replacer.repeat(spacesCount * depth - spacesCount);
+
+    const lines = Object
+      .entries(currentValue)
+      .map(([key, val]) => `${space}${key}: ${iter(val, depth + 1)}`)
+      .join('\n');
+    return `{\n${lines}\n${bracketSpace}}`;
+  };
+  return iter(value, startDepth);
+};
+
 const stylish = (value) => {
   const replacer = ' ';
   const spacesCount = 4;
@@ -9,33 +26,22 @@ const stylish = (value) => {
     const changedSpace = replacer.repeat(spacesCount * depth - 2);
     const bracketSpace = replacer.repeat(spacesCount * depth - spacesCount);
 
-    if (!_.isArray(currentValue)) {
-      if (!_.isObject(currentValue)) {
-        return `${currentValue}`;
-      }
-      const lines = Object
-        .entries(currentValue)
-        .map(([key, val]) => `${space}${key}: ${iter(val, depth + 1)}`)
-        .join('\n');
-      return `{\n${lines}\n${bracketSpace}}`;
-    }
-
     const lines = currentValue
       .map((node) => {
         switch (node.status) {
           case 'haveChildren':
             return `${space}${node.name}: ${iter(node.children, depth + 1)}`;
           case 'added':
-            return `${changedSpace}+ ${node.name}: ${iter(node.value, depth + 1)}`;
+            return `${changedSpace}+ ${node.name}: ${stringify(depth + 1, node.value, replacer, spacesCount)}`;
           case 'removed':
-            return `${changedSpace}- ${node.name}: ${iter(node.value, depth + 1)}`;
+            return `${changedSpace}- ${node.name}: ${stringify(depth + 1, node.value, replacer, spacesCount)}`;
           case 'updated':
             return [
-              `${changedSpace}- ${node.name}: ${iter(node.value1, depth + 1)}`,
-              `${changedSpace}+ ${node.name}: ${iter(node.value2, depth + 1)}`,
+              `${changedSpace}- ${node.name}: ${stringify(depth + 1, node.value1, replacer, spacesCount)}`,
+              `${changedSpace}+ ${node.name}: ${stringify(depth + 1, node.value2, replacer, spacesCount)}`,
             ].join('\n');
           default:
-            return `${space}${node.name}: ${node.value}`;
+            return `${space}${node.name}: ${stringify(depth + 1, node.value, replacer, spacesCount)}`;
         }
       }).join('\n');
     return `{\n${lines}\n${bracketSpace}}`;
